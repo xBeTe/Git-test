d04f43af02e872ce6b54eed8e68b322f16e0ad34
xBeTe <xbete1031@gmail.com>
2022-09-08 19:15:56 +0800

    first commit

---
+++ src/main/java/com/xbete/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xbete.git;
+
+/**
+ * @author xzxie
+ * @create 2022-09-08 19:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
